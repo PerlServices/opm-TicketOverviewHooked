1719dc4f1f552ab2d6eb64a466d09e7af2923850
reneeb <info@perl-services.de>
2016-09-30 17:23:03 +0200

    add hook for pending time

---
+++ Kernel/System/TicketOverview/Hooks/PendingTime.pm
@@ -0,0 +1,90 @@
+# --
+# Kernel/System/TicketOverview/Hooks/PendingTime.pm - mark tickets based on the queue in ticket overview
+# Copyright (C) 2013 - 2014 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::TicketOverview::Hooks::PendingTime;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = qw(
+    Kernel::Config
+    Kernel::System::Ticket
+);
+
+=head1 NAME
+
+Kernel::System::TicketOverview::Hooks::Junk - mark junk tickets in ticket overview
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+=item Run()
+
+Returns a color when the ticket belongs to the Junk-Queue
+
+    my $PendingTimeColor = $PendingTimeObject->Run(
+        TicketID  => 123,
+        UntilTime => 1351,
+    );
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(UntilTime)) {
+        if ( !$Param{$Needed} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    return if $Param{UntilTime} >= 0;
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $Color        = $ConfigObject->Get('Hook::PendingTime') || 'red';
+
+    return $Color;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
