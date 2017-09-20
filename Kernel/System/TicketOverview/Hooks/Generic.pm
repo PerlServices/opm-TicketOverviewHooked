6ff21e38cbad233afbbc581b09c7aac9cbbe235b
reneeb <info@perl-services.de>
2017-09-20 19:46:37 +0200

    add generic hook to avoid too many specialized hooks.
    With this generic hook it is possible to define the ticket attribute the hook is based on in the sysconfig.

---
+++ Kernel/System/TicketOverview/Hooks/Generic.pm
@@ -0,0 +1,96 @@
+# --
+# Copyright (C) 2017 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::TicketOverview::Hooks::Generic;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = qw(
+    Kernel::Config
+    Kernel::System::Ticket
+);
+
+=head1 NAME
+
+Kernel::System::TicketOverview::Hooks::Types
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+=item Run()
+
+Returns a color when the ticket belongs to the Junk-Queue
+
+    my $JunkColor = $JunkObject->Run(
+        TicketID => 123,
+    );
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $LogObject    = $Kernel::OM->Get('Kernel::System::Log');
+
+    my $Attribute = $ConfigObject->Get('Hook::Generic::Attribute');
+
+    # check needed stuff
+    for my $Needed ($Attribute) {
+        if ( !$Param{$Needed} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    my $Colors       = $ConfigObject->Get('Hook::Generic')       || {};
+    my $FontColors   = $ConfigObject->Get('Hook::Generic::Font') || {};
+
+    my $HookedColors = {
+        Font       => $FontColors->{ $Param{$Attribute} },
+        Background => $Colors->{ $Param{$Attribute} },
+    };
+
+    return $HookedColors;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
